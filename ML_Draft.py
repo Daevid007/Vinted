9c86cd30dae36d68a0129326ca1715a1de79e53a
Daevid007 <daviddms10@icloud.com>
2025-09-02 14:21:31 +0200

    Definitions File added and ML Draft

---
+++ ML_Draft.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  2 14:05:23 2025
+
+@author: david
+"""
+
+import Vinted_Definitions as vd
+
+
+data_raw = vd.load_data_parquet("test")
---
+++ Vinted_Definitions.py
@@ -0,0 +1,202 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep  2 14:12:56 2025
+
+@author: david
+"""
+
+
+import requests
+from PIL import Image
+from io import BytesIO
+import matplotlib.pyplot as plt
+from pyVinted import Vinted
+import pandas as pd
+import datetime
+import json
+import time
+import seaborn as sns
+from sklearn.preprocessing import OneHotEncoder
+import datetime
+from datetime import timedelta
+
+vinted = Vinted()
+
+def SearchVintedDraft(order = "relevance",price_to = "60",currency = "EUR",text = "Adidas-Vintage", page = 1):
+    """
+    Function to search on Vinted after specified search criteria, returning a list of items
+    Max number of items before Client error is around 950
+    """
+
+    string = "https://www.vinted.de/catalog?order="+order+"&price_to="+price_to+"&currency="+currency+"&search_text="+text
+    return vinted.items.search(string,900,page)
+
+
+
+def showproperties():
+    """
+    Function to show the raw data of one item searched, returning a dictionary
+    Purpose only is looking stuff up
+    """
+    return vinted.items.search("https://www.vinted.de/vetement?order=newest_first&price_to=60&currency=EUR&search_text=Adidas-Vintage",1,1)[0].raw_data
+
+def show_pic(url,title):
+    response = requests.get(url)
+    img = Image.open(BytesIO(response.content))
+
+    # Display image
+    plt.imshow(img)
+    plt.title(title)
+    plt.axis('off')  # Hide axes
+    plt.show()
+
+"""# Here define functions to work with the data parquet (which has to be down and uploaded manually in the notebook environment)"""
+
+def save_data_parquet(name, data):
+    """
+    Creates a new data parquet to store the data of found items
+    """
+    parquet_file_path = "C:/Users/David/OneDrive - fs-students.de/Vinted/Data/"+name+"_data_parquet" #HAS TO BE CHANGED EVENTUALLY DEPENDING ON DEVICE
+    data.to_parquet(parquet_file_path, index=False, compression='snappy')
+    print(f"\nDataFrame successfully saved to {parquet_file_path}")
+
+
+def load_data_parquet(name):
+    """
+    Loads a specified data_parquet into a dataframe and returns it
+    """
+    return pd.read_parquet("C:/Users/David/OneDrive - fs-students.de/Vinted/Data/"+name+"_data_parquet")  #HAS TO BE CHANGED EVENTUALLY DEPENDING ON DEVICE
+
+
+def add_data_to_parquet(name,data):
+    """
+    Adds search data to an existing data parquet, saves this parquet and returns the merged DataFrame
+    Also removes duplicates
+    """
+    tmp = pd.read_parquet("C:/Users/David/OneDrive - fs-students.de/Vinted/Data/"+name+"_data_parquet")
+    data = pd.concat([tmp,data])
+    parquet_file_path = "C:/Users/David/OneDrive - fs-students.de/Vinted/Data/"+name+"_data_parquet"   #HAS TO BE CHANGED EVENTUALLY DEPENDING ON DEVICE
+    data = data.drop_duplicates(subset=['ID'])
+    data.to_parquet(parquet_file_path, index=False, compression='snappy')
+    print(f"\nDataFrame successfully saved to {parquet_file_path}")
+    return data
+
+
+def create_search_df(items_searched):
+  df = pd.DataFrame({"ID":[],
+                    "Title": [],
+                    "Price":[],
+                    "Favourites":[],
+                    "Link":[],
+                    "Brand Title":[],
+                    "Promoted":[],
+                    "Status":[],
+                    "Fees":[],
+                    "Dates":[],
+                    "Photos":[]})
+  #------------------------------------------------------------------------------------------------------------------
+    
+
+
+  #storing the retrieved data in a df format
+
+  titles = [item.title for item in items_searched]
+  IDs = [item.id for item in items_searched]
+  prices = [item.price for item in items_searched]
+  favourites = [item.raw_data["favourite_count"] for item in items_searched]
+  Links = [item.url for item in items_searched]
+  Brands = [item.brand_title for item in items_searched]
+  Promoted = [item.raw_data["promoted"] for item in items_searched]
+  Status = [item.raw_data["status"] for item in items_searched]
+  Fees = [item.raw_data["service_fee"]["amount"] for item in items_searched]
+  Dates = [datetime.datetime.fromtimestamp(item.raw_timestamp) for item in items_searched]
+  photos = [item.photo for item in items_searched]
+  sizes = [item.size_title for item in items_searched]
+  
+  
+  df["ID"] = IDs
+  df["Title"] = titles
+  df["Price"] = prices
+  df["Favourites"] = favourites
+  df["Link"] = Links
+  df["Brand Title"] = Brands
+  df["Promoted"] = Promoted
+  df["Status"] = Status
+  df["Fees"] = Fees
+  df["Dates"] = Dates
+  df["Photos"] = photos
+  df["Size"] = sizes
+
+  return df
+
+
+
+
+def collect_data(order = "relevance",price_to = "60",currency = "EUR",parameters_text = ["Adidas-Vintage","Nike-Vintage"], pages = 1):
+  start_parquet_size = load_data_parquet("test").shape[0] #Saving start data size
+  print("Start parquet size:",start_parquet_size)
+  counter = 0
+  for i in (parameters_text): #We iterate trough the searched text parameters and...
+    counter += 1
+    for j in range(1,pages+1):
+      print("Parameter:",i, "Page:",j, "from",pages, "Parameter", counter, "from", len(parameters_text)) #Counter to keep track of progress
+      items_searched = SearchVintedDraft(order = order, price_to = price_to, currency = currency, text = i, page = j) #Simply search for the items
+      df = create_search_df(items_searched) #Creating the df for the searched items
+      #---------------------------------
+      current_time = datetime.datetime.now()
+      df["Time_Online"] = (current_time - df["Dates"])
+      df['Time_Online_H'] = df['Time_Online'].dt.total_seconds() / 3600
+      df = df.drop("Time_Online", axis=1)
+      df["Favourites_per_hour"] = df["Favourites"]/df["Time_Online_H"]
+      #---------------------------------
+      add_data_to_parquet("test",df) # Adding the data to the parquet
+      print(df.head()[["ID","Title"]])
+      print()
+      time.sleep(5) #To avoid errors...
+      #--------------- Here I should add some prevention so the web scraping does not go well or so
+
+  end_parquet_size = load_data_parquet("test").shape[0] #Saving end data size
+  print("End parquet size:",end_parquet_size) #Print end size
+  print("New data collected:",end_parquet_size-start_parquet_size) #Print how much was added
+
+
+
+def check_clean_data(name ="test"):
+    df = load_data_parquet(name)
+    print("Shape before:",df.shape)
+    df = df.drop_duplicates(subset=['ID'])
+    print("Shape:",df.shape)
+    return df
+
+
+def encode_cols(df):
+    # Create an instance of the encoder, drop='first' to avoid multicollinearity, sparse_output=False for dense array
+    Encoder = OneHotEncoder(drop='first', sparse_output=False)
+    
+    # Select the columns you want to use as features
+    features_df = df[["Title","Price", "Favourites", "Promoted", "Status", "Fees","Brand Title","Time_Online_H","Favourites_per_hour", "Size"]]
+    
+    # Use fit_transform on the instance to encode the 'Status' column
+    encoded_status = Encoder.fit_transform(features_df[["Status","Size","Brand Title"]])
+    
+    # Get the names of the encoded categories (excluding the dropped one)
+    encoded_category_names = Encoder.get_feature_names_out(["Status","Size","Brand Title"])
+    
+    # Create a DataFrame from the encoded status data with appropriate column names
+    encoded_status_df = pd.DataFrame(encoded_status, columns=encoded_category_names, index=features_df.index)
+    
+    # Drop the original 'Status' column from the features DataFrame
+    features_df = features_df.drop(["Status","Size","Brand Title"], axis=1)
+    
+    # Concatenate the original features (without 'Status') and the encoded status columns
+    data_encoded = pd.concat([features_df, encoded_status_df], axis=1)
+    
+    print(data_encoded.head())
+    
+    data_encoded['Promoted'] = data_encoded['Promoted'].astype(int)    
+    data_encoded['Price'] = pd.to_numeric(data_encoded['Price'], errors='coerce')
+    data_encoded['Fees'] = pd.to_numeric(data_encoded['Fees'], errors='coerce')
+    
+    return data_encoded
+
+
