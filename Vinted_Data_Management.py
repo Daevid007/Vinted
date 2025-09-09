2c972f0fae7f4ff9b43b82342281532fbb4196b4
Daevid007 <daviddms10@icloud.com>
2025-09-09 15:20:21 +0200

    Started connecting data management to github directly

--- Vinted_Data_Management.py
+++ Vinted_Data_Management.py
@@ -13,7 +13,7 @@ import Vinted_Definitions as vd
 name = "test2"
 
 #Creating Files
-create = True
+create = False
 if create == True:
     vd.create_data_parquet(name)
 
@@ -23,42 +23,44 @@ if delete == True:
     vd.delete_data_parquet(name)
 
 #Loading Files
-load = True
+load = False
 if load == True:
     loaded_data = vd.load_data_parquet(name)
     
 #Collecting Data
-collect = False
+list1 = ["Adidas-Vintage","Nike-Vintage","Reebook-Vintage","Puma-Vintage","WRSTBHVR","Kappa-Vintage",    "Puma Archive",
+"Reebok Classics",
+"Fila Vintage",
+"Champion Reverse Weave",
+"Ellesse Heritage",
+"Kappa Authentic",
+"Umbro Classics",
+"Diadora Heritage",
+"Lacoste Sport Vintage",
+"Levi's Vintage Clothing",
+"Carhartt WIP",
+"Patagonia Retro",
+"The North Face Vintage",
+"Columbia Sportswear Vintage",
+"Tommy Hilfiger Classics",
+"Ralph Lauren Polo Sport",
+"Converse Vintage",
+"Vans Classics",
+"New Balance Heritage",
+"Asics Onitsuka Tiger",
+"Starter Vintage",
+"Russell Athletic Vintage",
+"Nautica Competition",
+"Dickies Vintage",
+"Wrangler Retro",
+"Lee Jeans Vintage",
+"Timberland Heritage",
+"Jordan Brand Retro"]
+
+list2 = ["Adidas-Vintage"]
+collect = True
 if collect == True:
-    vd.collect_data(parameters_text = ["Adidas-Vintage","Nike-Vintage","Reebook-Vintage","Puma-Vintage","WRSTBHVR","Kappa-Vintage",    "Puma Archive",
-    "Reebok Classics",
-    "Fila Vintage",
-    "Champion Reverse Weave",
-    "Ellesse Heritage",
-    "Kappa Authentic",
-    "Umbro Classics",
-    "Diadora Heritage",
-    "Lacoste Sport Vintage",
-    "Levi's Vintage Clothing",
-    "Carhartt WIP",
-    "Patagonia Retro",
-    "The North Face Vintage",
-    "Columbia Sportswear Vintage",
-    "Tommy Hilfiger Classics",
-    "Ralph Lauren Polo Sport",
-    "Converse Vintage",
-    "Vans Classics",
-    "New Balance Heritage",
-    "Asics Onitsuka Tiger",
-    "Starter Vintage",
-    "Russell Athletic Vintage",
-    "Nautica Competition",
-    "Dickies Vintage",
-    "Wrangler Retro",
-    "Lee Jeans Vintage",
-    "Timberland Heritage",
-    "Jordan Brand Retro"])
-    
+    vd.collect_data(parameters_text = list2)
 #Check_cleaning
 check = False
 if check == True:
